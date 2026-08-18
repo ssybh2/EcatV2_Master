54c0d3a92ae5eeb67dc8b195f8b3a7b2878b3366
Boyang Hu <ssybh2@nottingham.edu.cn>
2026-08-18 14:16:57 +0800

    register 160-byte 6-IMU Large PDO module

--- src/soem_wrapper/src/ecat_node.cpp
+++ src/soem_wrapper/src/ecat_node.cpp
@@ -432,6 +432,7 @@ namespace aim::ecat {
         // register_module(2, "MotorModule", 56, 80, 001);
         register_module(3, "H750UniversalModule", 80, 80, 8);
         register_module(4, "H750UniversalModule (Large PDO V.)", 80, 112, 8);
+        register_module(5, "H750UniversalModule (6-IMU Large PDO V.)", 80, 160, 8);
     }
 
     void EthercatNode::register_module(const uint32_t eep_id,
